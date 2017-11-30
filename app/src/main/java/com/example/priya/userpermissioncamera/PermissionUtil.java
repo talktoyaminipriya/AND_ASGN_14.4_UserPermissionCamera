6b03eec86baaacf4dd66462eca23b6725289f896
talktoyaminipriya <talktoyaminipriya@gmail.com>
2017-11-30 23:57:33 +0530

    Update PermissionUtil.java

--- app/src/main/java/com/example/priya/userpermissioncamera/PermissionUtil.java
+++ app/src/main/java/com/example/priya/userpermissioncamera/PermissionUtil.java
@@ -10,29 +10,16 @@ import android.content.pm.PackageManager;
 
 
 
-/**
 
- * Utility class that wraps access to the runtime permissions API in M and provides basic helper
 
- * methods.
+ //Utility class that wraps access to the runtime permissions API in M and provides basic helper methods.
 
- */
+ 
 
 public abstract class PermissionUtil {
 
 
 
-    /**
-
-     * Check that all given permissions have been granted by verifying that each entry in the
-
-     * given array is of the value {@link PackageManager#PERMISSION_GRANTED}.
-
-     *
-
-     * @see Activity#onRequestPermissionsResult(int, String[], int[])
-
-     */
 
     public static boolean verifyPermissions(int[] grantResults) {
 
